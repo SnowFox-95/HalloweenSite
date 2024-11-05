d2c8c9eb16f5c7fe000ec78f4ba444fa97def9e9
SnowFox-95 <321mts95@gmail.com>
2024-11-05 22:20:09 +0300

    [X] footer section

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,32 @@
-import myswipe from './myswipe';
-import menu from './menu';
+import Swiper from "swiper";
+let menu = document.querySelector(".menu-icon");
 
-menu();
-myswipe();
+menu.onclick = () => {
+  menu.classList.toggle("move");
+};
+
+let swiper = new Swiper(".selling-content", {
+  spaceBetween: 20,
+  centeredSlides: false,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    280: { slidesPerView: 1, spaceBetween: 10 },
+    280: { slidesPerView: 2, spaceBetween: 10 },
+    750: { slidesPerView: 3, spaceBetween: 15 },
+    900: { slidesPerView: 4, spaceBetween: 20 },
+  },
+});
+
+let header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("header-active", window.scrollY > 0);
+});
