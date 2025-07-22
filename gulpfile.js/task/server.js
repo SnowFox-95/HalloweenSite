6675ac5bd6fbac6f67c84556058b0937dde4fea7
Alexey Bespyatykh <321mts95@gmail.com>
2025-07-22 23:46:30 +0300

    fix: disable brosersync for prod

--- gulpfile.js/task/server.js
+++ gulpfile.js/task/server.js
@@ -1,10 +1,18 @@
-//Сервер
-const server = ()=>{
-    $.browserSync.init({
-        server:{
-            baseDir: $.path.root
-        }
-    })
-}
+const server = (done) => {
+  if ($.app.isProd) {
+    done(); 
+    return;
+  }
+
+  $.browserSync.init({
+    server: {
+      baseDir: $.path.root,
+    },
+    open: false,
+    notify: false,
+  });
+
+  done();
+};
 
 module.exports = server;
